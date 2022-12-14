e4454568201eab447bb609417caa6188b4c4d7c0
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2022-12-14 18:12:08 +0400

    Update script.js

--- Ton_Ton/js/script.js
+++ Ton_Ton/js/script.js
@@ -4,7 +4,7 @@ let nowlang='EN';
 // let menuPage='';
   
 window.onload=()=>{
-//   nowlang=$('lang').value;
+//   nowlang=$('lang').value;--
   categorie.idCategoriesToHTML(categories[nowlang],$('categories'),categories['EN']);
   product.nowCategorie='tosts';
   product.idMain_menuToHTML(menu['tosts'],$('products_block'),nowlang,{
