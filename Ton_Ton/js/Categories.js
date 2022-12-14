1031cb65a9b22d264ae1531946bc1f253b884454
Mikael-Rt-JS <61776022+Mikael-Rt-JS@users.noreply.github.com>
2022-12-14 17:59:25 +0400

    Update Categories.js

--- Ton_Ton/js/Categories.js
+++ Ton_Ton/js/Categories.js
@@ -1,7 +1,11 @@
 class Categories{
   
   idCategoriesToHTML(categories,element,en){
-    let str="";
+    let str=`<select id="lang">
+      <option value="EN" selected="selected">EN</option>
+      <option value="RU">RU</option>
+      <option value="AM">AM</option>
+    </select>`;
     element.innerHTML='';
     for(let i=0;i<categories.length;i++){
       str+=`<li class="txt-upper" data-categorie="${en[i]}">${categories[i]}</li>`;
